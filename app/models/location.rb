1eae921f106269e886bbe26c80e50a8531f757fe
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-05 14:37:54 +0100

    Added validations for Location#latitude

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ApplicationRecord
   belongs_to :locationable, polymorphic: true
+
+  validates :latitude,
+            presence: true,
+            numericality: true,
+            inclusion: { in: -90 .. 90,
+                         message: "%{value} should be in the intervall -90 to 90" }
 end
