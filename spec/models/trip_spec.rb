444c67d69bc1e8edd87ea019b2011d5fd506b34c
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-05 16:15:11 +0100

    Added spec for valid case making sure we have at least 2 locations.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Trip, type: :model do
   subject { FactoryGirl.build :trip }
 
   it { is_expected.to be_valid }
+  
+  it "has at least 2 locations" do
+    expect(subject.locations.count).to be >= 2
+  end
 end
