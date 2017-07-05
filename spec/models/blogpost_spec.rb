6e6874235e138a3fc1a708b44c9100d412afe8c5
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-05 13:11:42 +0100

    Added validations to Blogpost#title

--- spec/models/blogpost_spec.rb
+++ spec/models/blogpost_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Blogpost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryGirl.build(:blogpost) }
+  it { is_expected.to be_valid }
+
+  it 'with blank title' do
+    invalid = FactoryGirl.build(:blogpost, title: '')
+    expect(invalid).not_to be_valid
+  end
 end
