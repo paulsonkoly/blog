1a9665ca8bb6fef3893a579dcbf26c7fa422c0aa
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-05 16:54:53 +0100

    Added spec to validate number of locations.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Trip, type: :model do
   it "has at least 2 locations" do
     expect(subject.locations.count).to be >= 2
   end
+
+  context "with less than 2 locations" do
+    subject { FactoryGirl.build :trip, location_count: 1 }
+
+    it { is_expected.not_to be_valid }
+  end
 end
