a621e1f6c61fac05224244b71e90d2f988662e32
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-05 14:41:52 +0100

    Added validations for Location#longitude.

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,4 +6,10 @@ class Location < ApplicationRecord
             numericality: true,
             inclusion: { in: -90 .. 90,
                          message: "%{value} should be in the intervall -90 to 90" }
+
+  validates :longitude,
+            presence: true,
+            numericality: true,
+            inclusion: { in: -180 .. 180,
+                         message: "%{value} should be in the intervall -180 to 180" }
 end
