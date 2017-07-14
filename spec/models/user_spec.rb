e5ff43bfdad55ce849d21388843a624754afa7b8
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-14 07:36:46 +0100

    Added user validations, and the bcrypt gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryGirl.build(:user) }
+
+  it { is_expected.to be_valid }
+  
+  it "is invalid with blank name" do
+    subject.name = ""
+    expect(subject).not_to be_valid
+  end
+
+  it "is expected to check name uniqueness" do
+    subject.save
+    subject2 = subject.dup
+    expect(subject2).not_to be_valid
+  end
 end
