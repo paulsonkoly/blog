fc8c4928ae3e23a57075a5b263734317760e1ea8
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-15 21:06:35 +0100

    Login/Logout links

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -15,4 +15,25 @@ RSpec.describe "layouts/application.html.erb", type: :view do
     render
     expect(rendered).to match 'Notice message'
   end
+
+  describe 'login/logout menu' do
+    context 'without a logged in user' do
+      it 'displays the log in link' do
+        render
+        expect(rendered).to match 'Login'
+      end
+    end
+
+    context 'with a logged in user' do
+      let (:user) { FactoryGirl.create :user }
+      before(:each) do
+        controller.helpers.login(user)
+      end
+    
+      it 'displays the logged in user\'s name' do
+        render
+        expect(rendered).to match "Logout #{user.name}"
+      end
+    end
+  end
 end
