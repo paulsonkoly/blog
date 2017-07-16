357d9777716f5923df655be61aca30e85b7088e5
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-16 18:45:47 +0100

    Added default scope to blogposts.

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -9,4 +9,6 @@ class Blogpost < ApplicationRecord
 
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  default_scope { order(created_at: :desc) }
 end
