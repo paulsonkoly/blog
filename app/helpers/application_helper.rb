3bc4255ace2d13394fc942b72e34346169f20153
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-26 09:25:43 +0100

    Added gravatar to comments.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
     @markdown.render(text).html_safe
   end
+
+  # generates the gravatar img url from the user's email.
+  #
+  #   <%= image_tag gravatar_url("you@youremail.com", 64), alt: "" %>
+  #
+  def gravatar_url(email, size)
+    gravatar = Digest::MD5::hexdigest(email).downcase
+    "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+  end
 end
