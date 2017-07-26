f7edc412b355ae7986b2e81c80b7a75c6408f2c1
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-26 09:46:36 +0100

    Added comment save errors display.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  include ActionView::Helpers::TextHelper
+  
   load_and_authorize_resource
   
   def create
@@ -9,7 +11,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @comment.blogpost, notice: 'Your comment was succesfully sent to the server.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { redirect_to @comment.blogpost, alert: 'Your comment couldn\'t be saved.' }
+        format.html { redirect_to @comment.blogpost, alert: comment_error_block(@comment) }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -19,5 +21,15 @@ class CommentsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
     params.require(:comment).permit(:content, :name, :email)
+  end
+
+  def comment_error_block(comment)
+    output = "<h2>#{pluralize(comment.errors.count, "error")} prohibited this comment from being saved:</h2>\n"
+    output << "<ul>\n"
+    comment.errors.full_messages.each do |message|
+      output << "<li>#{message}</li>\n"
+    end       
+    output << "</ul>\n"
+    output
   end
 end
