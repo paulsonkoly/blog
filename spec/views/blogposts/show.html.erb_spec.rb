f9c2def6cf6d243c611cc457cd94285433766617
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-07-27 22:23:35 +0100

    fixed double <p> in comment blocks and added specs for it

--- spec/views/blogposts/show.html.erb_spec.rb
+++ spec/views/blogposts/show.html.erb_spec.rb
@@ -17,11 +17,27 @@ RSpec.describe "blogposts/show", type: :view do
 
   it "renders the comment form" do
     render
-    
+
     expect(rendered).to have_form(blogpost_comments_path(blogpost_id: @blogpost.to_param), :post) do
       with_tag 'input', with: { name: 'comment[name]' }
       with_tag 'input', with: { name: 'comment[email]' }
       with_tag 'textarea', with: { name: 'comment[content]' }
+    end
+  end
+
+  context 'with a comment' do
+    before (:each) do
+      @blogpost.comments.create(FactoryGirl.attributes_for :comment, content: 'hello')
+    end
+
+    it 'renders the comments' do
+      render
+      expect(rendered).to match("hello")
+    end
+
+    it 'doesn\'t output double <p> tags' do
+      render
+      expect(rendered).not_to match('<p><p>')
     end
   end
 end
