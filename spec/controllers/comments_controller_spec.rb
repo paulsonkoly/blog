8e8e3f9a68ac534a2c611e561491742a2b40e213
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-09-23 11:29:07 +0100

    rubocop controller specs

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
   let(:blogpost) { FactoryGirl.create :blogpost }
-  
+  let(:comment) { FactoryGirl.create :comment, blogpost: blogpost }
+
   let(:valid_attributes) do
     FactoryGirl.attributes_for(:comment)
   end
@@ -11,69 +14,67 @@ RSpec.describe CommentsController, type: :controller do
     FactoryGirl.attributes_for(:comment).merge(email: '')
   end
 
-  before (:each) do
-    @comment = FactoryGirl.create :comment, blogpost: blogpost
-  end
-
   let(:logged_in_user) { FactoryGirl.create :user }
   let(:logged_in_session) { { user_id: logged_in_user.id } }
-  
-  describe "POST #create" do
-    it "creates a new Comment" do
-      expect {
-        post :create,
-             params: { comment: valid_attributes, blogpost_id: blogpost.to_param },
-             session: {}
-      }.to change(Comment, :count).by(1)
+
+  describe 'POST #create' do
+    it 'creates a new Comment' do
+      expect do
+        post(:create,
+             params: { comment: valid_attributes,
+                       blogpost_id: blogpost.to_param },
+             session: {})
+      end.to change(Comment, :count).by(1)
     end
 
     context 'with invalid params' do
-      it "redirects to Blogpost#show" do
-        post :create, params: {comment: invalid_attributes , blogpost_id: blogpost.to_param}, session: {}
+      it 'redirects to Blogpost#show' do
+        post(:create,
+             params: { comment: invalid_attributes,
+                       blogpost_id: blogpost.to_param },
+             session: {})
         expect(response).to redirect_to(blogpost)
       end
     end
   end
 
-  describe "GET #edit" do
-    it "returns a success response" do
-      get :edit,
-          params: {id: @comment.to_param, blogpost_id: blogpost.to_param},
-          session: logged_in_session
+  describe 'GET #edit' do
+    it 'returns a success response' do
+      get(:edit,
+          params: { id: comment.to_param, blogpost_id: blogpost.to_param },
+          session: logged_in_session)
       expect(response).to be_success
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        valid_attributes.merge content: 'hello'
-      }
+  describe 'PUT #update' do
+    context 'with valid params' do
+      let(:new_attributes) { valid_attributes.merge content: 'hello' }
 
-      it "updates the requested comment" do
+      it 'updates the requested comment' do
         put :update,
-            params: { id: @comment.to_param,
+            params: { id: comment.to_param,
                       blogpost_id: blogpost.to_param,
                       comment: new_attributes },
             session: logged_in_session
-        @comment.reload
-        expect(@comment.content).to eq('hello')
+        comment.reload
+        expect(comment.content).to eq('hello')
       end
 
-      it "redirects to the blogpost" do
+      it 'redirects to the blogpost' do
         put :update,
-            params: { id: @comment.to_param,
+            params: { id: comment.to_param,
                       blogpost_id: blogpost.to_param,
                       comment: valid_attributes },
             session: logged_in_session
-        expect(response).to redirect_to(@comment.blogpost)
+        expect(response).to redirect_to(comment.blogpost)
       end
     end
 
-    context "with invalid params" do
+    context 'with invalid params' do
       it "returns a success response (i.e. to display the 'edit' template)" do
         put :update,
-            params: { id: @comment.to_param,
+            params: { id: comment.to_param,
                       blogpost_id: blogpost.to_param,
                       comment: invalid_attributes },
             session: logged_in_session
@@ -82,18 +83,19 @@ RSpec.describe CommentsController, type: :controller do
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested comment" do
-      expect {
+  describe 'DELETE #destroy' do
+    it 'destroys the requested comment' do
+      comment.save!
+      expect do
         delete :destroy,
-               params: { id: @comment.to_param, blogpost_id: blogpost.to_param},
+               params: { id: comment.to_param, blogpost_id: blogpost.to_param },
                session: logged_in_session
-      }.to change(Comment, :count).by(-1)
+      end.to change(Comment, :count).by(-1)
     end
 
-    it "redirects to the blogpost" do
+    it 'redirects to the blogpost' do
       delete :destroy,
-             params: { id: @comment.to_param, blogpost_id: blogpost.to_param},
+             params: { id: comment.to_param, blogpost_id: blogpost.to_param },
              session: logged_in_session
       expect(response).to redirect_to(blogpost)
     end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,33 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  let (:user) { FactoryGirl.create(:user) }
-  
-  describe "GET #new" do
-    it "returns http success" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to be_success
     end
   end
 
-  describe "POST #create" do
+  describe 'POST #create' do
     context 'with non existent user' do
       it 'returns http success' do
-        post :create, { params: { session: { user_name: 'nonexistent', password: '' } } }
+        post(:create,
+             params: { session: { user_name: 'nonexistent', password: '' } })
         expect(response).to be_success
       end
     end
-    
+
     context 'with invallid credentials' do
       before do
         post :create, params: { session: { user_name: user.name,
                                            user_password: 'something' } }
       end
-      
+
       it 'doesn\'t redirect' do
         expect(response).not_to be_a_redirect
       end
-      
+
       it "doesn't set the current user" do
         expect(controller.current_user).to be_nil
       end
@@ -42,21 +45,21 @@ RSpec.describe SessionsController, type: :controller do
       it 'redirects to the root url' do
         expect(response).to redirect_to(root_url)
       end
-      
+
       it 'sets current_user' do
         expect(controller.current_user).not_to be_nil
       end
     end
   end
 
-  describe "DELETE #destroy" do
-    it "removes the current user" do
+  describe 'DELETE #destroy' do
+    it 'removes the current user' do
       controller.login(user)
-      expect { delete :destroy }.to change { controller.current_user }.to(nil)
+      expect { delete :destroy }.to change(controller, :current_user).to(nil)
     end
 
     it 'redirects to the root url' do
-      delete :destroy      
+      delete :destroy
       expect(response).to redirect_to(root_url)
     end
   end
